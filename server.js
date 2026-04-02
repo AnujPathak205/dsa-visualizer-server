eee841f7969c7a50b7239403b199b653d052f72e
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2026-04-02 10:00:00 +0530

    basic setup of app done

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+
+const PORT = process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`app is running in port ${PORT}`);
+})
